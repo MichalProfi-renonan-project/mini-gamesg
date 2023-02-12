095fbbd6194a3a40aeb5d739638c6de920f7c81a
MartinHolena <martinkiko18@gmail.com>
2023-02-12 18:59:34 +0100

    add Start button and navigation between MainWindow and Page1

--- tic_tac_toe/Window1.xaml.cs
+++ tic_tac_toe/Window1.xaml.cs
@@ -10,28 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace tic_tac_toe
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Page1 page1 = new Page1();
-            this.Content = page1;
-        }
-
     }
 }
