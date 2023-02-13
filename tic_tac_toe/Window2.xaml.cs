f05fdbdf0960d033b7a96c634b62cf2ef28bc0d5
MartinHolena <martinkiko18@gmail.com>
2023-02-13 19:01:02 +0100

    add the second window for choosing games

--- tic_tac_toe/Window2.xaml.cs
+++ tic_tac_toe/Window2.xaml.cs
@@ -10,32 +10,26 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using tic_tac_toe2;
 
 namespace tic_tac_toe
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window2.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window2 : Window
     {
-
-        public MainWindow()
+        public Window2()
         {
             InitializeComponent();
-
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_tictactoe_Click(object sender, RoutedEventArgs e)
         {
-            this.Hide();
-            Window2 win = new Window2();
-            win.Show();
 
-         
+            Page1 page1 = new Page1();
+            this.Content = page1;
+
         }
-
     }
 }
