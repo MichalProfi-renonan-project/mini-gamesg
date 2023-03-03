f5a855f71ce61e16aa1d2f1cf1297611a2031412
MartinHolena <martinkiko18@gmail.com>
2023-03-03 15:17:29 +0100

    add Snake version 0.2 - don´t work

---
+++ tic_tac_toe/Start Menu/games/SnakeElement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace tic_tac_toe
+{
+    class SnakeElement
+    {
+        public SnakeElement(int Size)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Width = Size;
+            rect.Height = Size;
+            rect.Fill = Brushes.Green;
+            UIElement = rect;
+        }
+        
+        public UIElement UIElement { get; set; }
+        public bool IsHead { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
--- tic_tac_toe/Start Menu/games/SnakeGame.xaml.cs
+++ tic_tac_toe/Start Menu/games/SnakeGame.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace tic_tac_toe
 {
@@ -19,16 +20,135 @@ namespace tic_tac_toe
     /// </summary>
     public partial class SnakeGame : Window
     {
+        int _elementSize = 20;
+        private int _numberOfColumns;
+        private int _numberOfRows;
+
+        DispatcherTimer _gameLoopTimer;
+        List<SnakeElement> _snakeElements;
+        
+        private Direction _currentDirection;
         public SnakeGame()
         {
             InitializeComponent();
+            InitializeTimer();
+            DrawGameWorld();
+            InitializeSnake();
+            DrawSnake();
         }
 
-        private void Button_back_Click(object sender, RoutedEventArgs e)
+        private void DrawSnake()
         {
-            this.Close();
-            ChoosingGame back = new ChoosingGame();
-            back.Show();
+            foreach (var snakeElement in _snakeElements)
+            {
+                if (!GameWorld.Children.Contains(snakeElement.UIElement))
+                    GameWorld.Children.Add(snakeElement.UIElement);
+
+                Canvas.SetLeft(snakeElement.UIElement, snakeElement.X);
+                Canvas.SetTop(snakeElement.UIElement, snakeElement.Y);
+            }
         }
+
+        private void InitializeSnake()
+        {
+            _snakeElements = new List<SnakeElement>();
+            _snakeElements.Add(new SnakeElement(_elementSize)
+            {
+                X = (_numberOfRows / 2) * _elementSize,
+                Y = (_numberOfColumns / 2) * _elementSize
+            });
+            _currentDirection = Direction.Left;
+        }
+
+        private void DrawGameWorld()
+        {
+            _numberOfColumns = (int) this.Width / _elementSize;
+            _numberOfRows = (int) this.Height / _elementSize;
+
+            for (int i = 0; i < _numberOfRows; i++)
+            {
+                Line line = new Line();
+                line.Stroke = Brushes.Black;
+                line.X1 = 0;
+                line.Y1 = i * _elementSize;
+                line.X2 = Width;
+                line.Y2 = i * _elementSize;
+                GameWorld.Children.Add(line);
+            }
+            for (int i = 0; i < _numberOfColumns; i++)
+            {
+                Line line = new Line();
+                line.Stroke = Brushes.Black;
+                line.X1 = i * _elementSize;
+                line.Y1 = 0;
+                line.X2 = i * _elementSize;
+                line.Y2 = Height;
+                GameWorld.Children.Add(line);
+            }
+        }
+
+        public void InitializeTimer()
+        {
+            _gameLoopTimer = new DispatcherTimer();
+            _gameLoopTimer.Interval = TimeSpan.FromSeconds(0.5);
+            _gameLoopTimer.Tick += MainGameLoop;
+            _gameLoopTimer.Start();
+        }
+
+        private void MainGameLoop(object sender, EventArgs e)
+        {
+            MoveSnake();
+            DrawSnake();
+        }
+        private void MoveSnake()
+        {
+            foreach (var snakeElement in _snakeElements)
+            {
+                switch (_currentDirection)
+                {
+                    case Direction.Right:
+                        snakeElement.X -= _elementSize;
+                        break;
+                    case Direction.Left:
+                        snakeElement.X += _elementSize;
+                        break;
+                    case Direction.Up:
+                        snakeElement.Y += _elementSize;
+                        break;
+                    case Direction.Down:
+                        snakeElement.Y -= _elementSize;
+                        break;
+
+                }
+
+                
+            }
+
+        }
+
+        private void KeyRealised(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.W:
+                    _currentDirection = Direction.Up;
+                    break;
+                case Key.A:
+                    _currentDirection = Direction.Left;
+                    break;
+                case Key.S:
+                    _currentDirection = Direction.Down;
+                    break;
+                case Key.D:
+                    _currentDirection = Direction.Right;
+                    break;
+            }
+        }
+    }
+    enum Direction{
+        Right,
+        Left,
+        Up,
+        Down
     }
 }
